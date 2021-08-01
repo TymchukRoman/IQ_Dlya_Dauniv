46cd8af122e09bcfefe45ad8fe4312f700314913
RomanIvanytskyi <ivanitckij999@gmail.com>
2021-08-01 18:01:03 +0300

    front-4

--- front/src/Axios/api.js
+++ front/src/Axios/api.js
@@ -1,15 +1,21 @@
 import axios from "axios";
 
-export const getData = () => {
-  return axios.get("http://localhost:4000/getData");
-};
+const client = axios.create({
+    baseURL: "http://localhost:3000/",
+});
+
+// export const getData = () => {
+//   return axios.get("http://localhost:4000/getData");
+// };
 
 export const postData = (data) => {
-  return axios.post("http://localhost:4000/postData", {
-    question: data.qText,
-    right_answer: data.rigthAnswer,
-    answers_list : data.answerList, 
-    postId: data.postId,
-  });
-};
-
+    let array = [data.answersList0, data.answersList1, data.answersList2, data.answersList3]
+    return client.post("addQuestion", {
+      qText: data.qText,
+      rigthAnswer: data.rigthAnswer,
+      answerList: [...array],
+      login: data.login,
+      password: data.password
+      
+    });
+}
