54f007ee7860392d80c78ab543859f29f98d0c17
RomanIvanytskyi <ivanitckij999@gmail.com>
2021-08-02 20:09:28 +0300

    front-5(Header fix)

--- front/src/Pages/Layouts/Routers/Layouts.jsx
+++ front/src/Pages/Layouts/Routers/Layouts.jsx
@@ -5,24 +5,25 @@ import {
   } from "react-bootstrap";
   import { Link } from "react-router-dom";
   import paths from "./paths";
+  import classes from "./Layouts.module.css"
   
   const Layout = ({ children }) => {
     return (
       <div>
         <Navbar bg="dark" expand="lg">
-          <Container>
-            <Navbar.Brand as={Link} to={paths.main}>
-              IQ
+          <Container >
+            <Navbar.Brand as={Link} to={paths.main} >
+              <p className={classes.NavLinks}>IQ </p>
             </Navbar.Brand>
             <Nav className="mr-auto">
               <Nav.Link as={Link} to={paths.test}>
-              Tests
+              <p className={classes.NavLinks}>Tests</p>
               </Nav.Link>
               <Nav.Link as={Link} to={paths.results}>
-                Results
+              <p className={classes.NavLinks}> Results</p>
               </Nav.Link>
               <Nav.Link as={Link} to={paths.add}>
-                AddTest
+              <p className={classes.NavLinks}> AddTest</p>
               </Nav.Link>
             </Nav>
           </Container>
