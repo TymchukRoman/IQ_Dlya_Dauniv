309d7f301418e1344be2761a57a3c6cb4980ac1d
TymchukRoman <roman.tymchuk.t@gmail.com>
2021-08-29 03:12:11 +0300

    front-21 remaked leaderboards page

--- front/src/Pages/Leaderboards.jsx
+++ front/src/Pages/Leaderboards.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { getLeaderboards } from "../Axios/api";
-
+import classes from "./styles/Top.module.css"
 
 const Leaderboards = () => {
 
@@ -13,11 +13,23 @@ const Leaderboards = () => {
   }, [])
 
   return <div>
-    {leaderboards.map((item) => {
-      return <p key={item.name}>
-        {item.name} Score: {item.points}
-      </p>
-    })}
+    {leaderboards.length
+      ? <div>
+        <div className={classes.first + " " + classes.topitem}>
+          <p><h4>{leaderboards[0].name}</h4></p>
+          <p> Total score: {leaderboards[0].points}</p>
+        </div>
+        <div className={classes.second + " " + classes.topitem}>
+          <p><h4>{leaderboards[1].name}</h4></p>
+          <p>Total score: {leaderboards[1].points}</p>
+        </div>
+        <div className={classes.third + " " + classes.topitem}>
+          <p><h4>{leaderboards[2].name}</h4></p>
+          <p>Total score: {leaderboards[2].points}</p>
+        </div>
+      </div>
+      : <p>Wait for data...</p>
+    }
   </div>;
 };
 export default Leaderboards;
